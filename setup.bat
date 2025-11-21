2f90e4054d2ae9ac7050b84aa3727cda2ed28c9b
23jrg <jrg71442@yahoo.com>
2025-11-21 10:36:42 -0500

    Update setup.bat
    Added the option to uninstall the .bat from the LogOffInactive folder

--- setup.bat
+++ setup.bat
@@ -2,5 +2,7 @@ pushd %~dp0
 if exist "C:\LogOffInactive" (@RD /S /Q "C:\LogOffInactive")
 mkdir C:\LogOffInactive
 copy /y "LogInactiveOff.ps1" "C:\LogOffInactive"
+copy /y "uninstall.bat" "C:\LogOffInactive"
 schtasks.exe /Create /XML "Log off inactive users.xml" /tn LogInactiveOff
+
 popd
