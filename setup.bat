9c851aec97d2a2926ada9bdcb1d511280091df01
23jrg <jrg71442@yahoo.com>
2025-11-21 11:11:17 -0500

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,8 +1,10 @@
-pushd %~dp0
 if exist "C:\LogOffInactive" (@RD /S /Q "C:\LogOffInactive")
 mkdir C:\LogOffInactive
+pushd %~dp0
 copy /y "LogInactiveOff.ps1" "C:\LogOffInactive"
 copy /y "uninstall.bat" "C:\LogOffInactive"
+popd
 schtasks.exe /Create /XML "Log off inactive users.xml" /tn LogInactiveOff
 
-popd
+
+
