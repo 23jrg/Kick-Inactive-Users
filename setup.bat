8103c1d355a5e7872486b5b10641bbc0f165e59f
23jrg <jrg71442@yahoo.com>
2025-11-21 11:43:03 -0500

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,9 +1,23 @@
+# Checks to see if the script was already ran and removes leftover files
 if exist "C:\LogOffInactive" (@RD /S /Q "C:\LogOffInactive")
+
+# Creates the resource folder
 mkdir C:\LogOffInactive
+
+# Changed the path to the working directory
 pushd %~dp0
+
+# Moves the powershell script and scheduled task to the resources folder
 copy /y "LogInactiveOff.ps1" "C:\LogOffInactive"
 copy /y "Log off inactive users.xml" "C:\LogOffInactive"
-copy /y "uninstall.bat" "C:\LogOffInactive"
+
+# Creates a shortcut to the uninstaller so the uninstaller can remove the resource folder without being inside it
+powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('c:\LogOffInactive\uninstall.lnk');$s.TargetPath='%userprofile%\Kick-Inactive-Users\uninstall.bat';$s.Save()"
+
+# Resets the path to default
 popd
+
+#Creates the scheduled task to automatically trigger when someone logs in
 schtasks.exe /Create /XML "C:\Log off inactive users.xml" /tn LogInactiveOff
+
 
