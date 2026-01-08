5fade8a3f256f052093c0f17ad9a89889fad2f8c
23jrg <jrg71442@yahoo.com>
2026-01-08 10:11:02 -0500

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -11,6 +11,13 @@ REM Moves the powershell script and scheduled task to the resources folder
 copy /y "LogInactiveOff.ps1" "C:\LogInactiveOff"
 copy /y "Log off inactive users.xml" "C:\LogInactiveOff"
 
+$WshShell = New-Object -COMObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut("$Home\Desktop\Uninstall_LogOffInactiveAccounts.lnk")
+$Shortcut.TargetPath = "C:\23jrg\MediaCreationTool.bat\MediaCreationTool.bat"
+$Shortcut.Save()
+
+if exist "%home\desktop\Install_LogOffInactiveAccounts.lnk" (@RD /S /Q "%home\desktop\Install_LogOffInactiveAccounts.lnk")
+
 REM Creates a shortcut to the uninstaller so the uninstaller can remove the resource folder without being inside it
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('c:\LogOffInactive\uninstall.lnk');$s.TargetPath='%tmp%\Kick-Inactive-Users\uninstall.bat';$s.Save()"
 
@@ -19,6 +26,7 @@ popd
 
 REM Creates the scheduled task to automatically trigger when someone logs in
 schtasks.exe /Create /XML "C:\LogOffInactive\Log off inactive users.xml" /tn LogInactiveOff
+
 
 
 
