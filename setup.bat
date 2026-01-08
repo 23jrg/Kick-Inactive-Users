552e3f50a6d94c980296fd4f6ba8660f88107717
23jrg <jrg71442@yahoo.com>
2026-01-08 10:38:03 -0500

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -12,24 +12,16 @@ copy /y "LogInactiveOff.ps1" "C:\LogInactiveOff"
 copy /y "uninstall.bat" "C:\LogInactiveOff"
 copy /y "Log off inactive users.xml" "C:\LogInactiveOff"
 
-$WshShell = New-Object -COMObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut("$Home\Desktop\Uninstall_LogOffInactiveAccounts.lnk")
-$Shortcut.TargetPath = "C:\LogInactiveOff\uninstall.bat"
-$Shortcut.Save()
 
-if exist "%home\desktop\Install_LogOffInactiveAccounts.lnk" (@RD /S /Q "%home\desktop\Install_LogOffInactiveAccounts.lnk")
+DEL "%userprofile%\desktop\Install_LogOffInactiveAccounts.Ink"
 
 REM Creates a shortcut to the uninstaller so the uninstaller can remove the resource folder without being inside it
-powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('c:\LogOffInactive\uninstall.lnk');$s.TargetPath='%tmp%\Kick-Inactive-Users\uninstall.bat';$s.Save()"
+
+mklink C:\Users\jgraham\Desktop\Uninstall_LogOffInactiveAccounts.lnk C:\LogInactiveOff\uninstall.bat
 
 REM Resets the path to default
 popd
 
 REM Creates the scheduled task to automatically trigger when someone logs in
 schtasks.exe /Create /XML "C:\LogOffInactive\Log off inactive users.xml" /tn LogInactiveOff
-
-
-
-
-
 
