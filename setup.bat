556b5150f0f68f952ab1e982b808197a653481bb
23jrg <jrg71442@yahoo.com>
2026-01-08 10:06:44 -0500

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,18 +1,18 @@
 REM Checks to see if the script was already ran and removes leftover files
-if exist "C:\LogOffInactive" (@RD /S /Q "C:\LogOffInactive")
+if exist "C:\LogInactiveOff" (@RD /S /Q "C:\LogInactiveOff")
 
 REM Creates the resource folder
-mkdir C:\LogOffInactive
+mkdir C:\LogInactiveOff
 
 REM Changed the path to the working directory
 pushd %~dp0
 
 REM Moves the powershell script and scheduled task to the resources folder
-copy /y "LogInactiveOff.ps1" "C:\LogOffInactive"
-copy /y "Log off inactive users.xml" "C:\LogOffInactive"
+copy /y "LogInactiveOff.ps1" "C:\LogInactiveOff"
+copy /y "Log off inactive users.xml" "C:\LogInactiveOff"
 
 REM Creates a shortcut to the uninstaller so the uninstaller can remove the resource folder without being inside it
-powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('c:\LogOffInactive\uninstall.lnk');$s.TargetPath='%userprofile%\Kick-Inactive-Users\uninstall.bat';$s.Save()"
+powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('c:\LogOffInactive\uninstall.lnk');$s.TargetPath='%tmp%\Kick-Inactive-Users\uninstall.bat';$s.Save()"
 
 REM Resets the path to default
 popd
