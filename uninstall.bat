671820bcecdb54405b77bfeac7046418226d4211
23jrg <jrg71442@yahoo.com>
2026-01-08 10:47:15 -0500

    Update uninstall.bat

--- uninstall.bat
+++ uninstall.bat
@@ -1,3 +1,3 @@
 schtasks /delete /tn LogInactiveOff /f
 if exist "C:\LogOffInactive" (@RD /S /Q "C:\LogOffInactive")
-del "%userprofile%\desktop\Uninstall_LogOffInactiveAccounts.Ink"
+del %userprofile%\desktop\Uninstall_LogOffInactiveAccounts.lnk"
