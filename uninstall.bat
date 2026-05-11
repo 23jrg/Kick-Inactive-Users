acb2d4dc1ea8f060ab05677f7c2463737399080e
23jrg <jrg71442@yahoo.com>
2026-05-11 15:32:29 -0400

    Update uninstall.bat

--- uninstall.bat
+++ uninstall.bat
@@ -1,3 +1,8 @@
+FSUTIL DIRTY query %SystemDrive% >NUL || (
+    PowerShell "Start-Process -FilePath '%0' -Verb RunAs"
+    EXIT
+)
+
 schtasks /delete /tn LogInactiveOff /f
 rmdir /S /Q C:\LogInactiveOff
 del %userprofile%\desktop\Uninstall_LogOffInactiveAccounts.lnk
