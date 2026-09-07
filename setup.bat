7eb0294132d37dd4f6f93f6bec4b2dd7f2e77e1b
23jrg <jrg71442@yahoo.com>
2026-09-07 01:17:37 -0400

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -13,21 +13,13 @@ REM Changed the path to the working directory
 pushd %~dp0
 
 REM Moves the powershell script and scheduled task to the resources folder
-copy /y "LogInactiveOff.ps1" "C:\LogInactiveOff"
 copy /y "uninstaller.bat" "C:\LogInactiveOff"
-copy /y "Log off inactive users.xml" "C:\LogInactiveOff"
-mklink %userprofile%\Desktop\Uninstall_LogOffInactiveAccounts.lnk %tmp%\KIU\uninstall.bat
-
-DEL "%userprofile%\desktop\Install_LogOffInactiveAccounts.lnk"
-
-REM Creates a shortcut to the uninstaller so the uninstaller can remove the resource folder without being inside it
-
-
+copy /y "LogInactiveOff.xml" "C:\LogInactiveOff"
 
 REM Resets the path to default
 popd
 
 REM Creates the scheduled task to automatically trigger when someone logs in, run in powershell to avoid false positives
-powershell -Command "schtasks.exe /Create /XML 'C:\LogInactiveOff\Log off inactive users.xml' /tn LogInactiveOff"
+powershell -Command "schtasks.exe /Create /XML 'C:\LogInactiveOff\LogInactiveOff.xml' /tn LogInactiveOff"
 
 
